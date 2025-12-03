90252c06b365f07534cbacdb34bfd9c2fc45af78
Felipe Torolho <felipetorolho@gmail.com>
2025-12-03 16:47:09 -0300

    Começando o backend do DevConnect

---
+++ MVC/DevConnect/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DevConnect.Models;
+
+// CADASTRO DE ÚSUARIO
+namespace DevConnect.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        public UsuarioController(ILogger<UsuarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
